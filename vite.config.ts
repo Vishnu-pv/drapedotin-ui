459add98ff83a82f07d2208912dd18e1b25cad86
Vishnu P V <vishnupv00143@gmail.com>
2023-09-20 00:08:35 +0530

    Added Express Server

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import express from './express-plugin'
  
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), express('src/server')],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
